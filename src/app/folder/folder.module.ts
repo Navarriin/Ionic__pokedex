0e3e11b934576ec655c3fd661d76ef1add7ec7f5
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-09 22:19:05 -0300

    criaçao de projeto e organização

--- src/app/folder/folder.module.ts
+++ src/app/folder/folder.module.ts
@@ -7,14 +7,10 @@ import { IonicModule } from '@ionic/angular';
 import { FolderPageRoutingModule } from './folder-routing.module';
 
 import { FolderPage } from './folder.page';
+import { PokemonListComponent } from '../components/pokemon-list/pokemon-list.component';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    FolderPageRoutingModule
-  ],
-  declarations: [FolderPage]
+  imports: [CommonModule, FormsModule, IonicModule, FolderPageRoutingModule],
+  declarations: [FolderPage, PokemonListComponent],
 })
 export class FolderPageModule {}
