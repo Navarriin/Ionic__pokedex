b105b5a704fcf57ace975c305d688e07cc0aaaf4
Gabriel <gabrielnavarro.vip@gmail.com>
2023-12-11 13:35:33 -0300

    removendo labels

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,6 +8,6 @@ export class AppComponent {
   public appPages = [
     { title: 'Pokemons', url: '/folder/inbox', icon: 'logo-angular' },
   ];
-  public labels = ['Pokemons', 'Testes'];
+  
   constructor() {}
 }
